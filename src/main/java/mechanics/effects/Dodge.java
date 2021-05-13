fdce8a94a29798e44cb93a199730b2be32627f08
Sbyner <tmmscrltt@gmail.com>
2021-05-13 19:26:05 +0200

    Cringe

--- src/main/java/mechanics/effects/Dodge.java
+++ src/main/java/mechanics/effects/Dodge.java
@@ -1,24 +1,28 @@
 package mechanics.effects;
 
 import creatures.Creature;
+import mechanics.Action;
 
 public class Dodge extends Effect{
 	int durationTurns;
+	
 	Dodge(int durationTurns){
 		this.durationTurns=durationTurns;
+		trigger = onEvent.ACTION;
 	}
 	@Override
 	public void tick() {
 		durationTurns--;
 		
 	}
-
 	@Override
-	public void accept(Creature arg0) {
+	public void accept(Creature arg0, Action arg1) {
 		if(durationTurns>0) {
-			arg0.
+			
 		}
 		
 	}
+
+
 
 }
--- src/main/java/mechanics/effects/Effect.java
+++ src/main/java/mechanics/effects/Effect.java
@@ -6,6 +6,7 @@ import creatures.Creature;
 import mechanics.Action;
 
 public abstract class Effect implements BiConsumer<Creature,Action>{
+	onEvent trigger;
 	public void tick() {}
 
 	public enum onEvent {
